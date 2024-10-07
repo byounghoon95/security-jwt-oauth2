fa9ac59cc46cd7841196345af7cb729a5acd5d1c
owner <toltolyibh@gmail.com>
2024-10-07 21:42:35 +0900

    init git commit -m init

---
+++ jwt-back/src/test/java/com/example/jwtback/JwtBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
